5718b66594b69373f69752eb31a696334b1c9c3c
YulllJ <jyr8407@gmail.com>
2024-01-15 10:56:02 +0900

    V0.1

---
+++ day02.py
@@ -0,0 +1,12 @@
+# def main():
+#     print(5)
+#
+# if __name__ =="__main__":
+#     main()
+
+subjects=['python', 'linux','data structure','database']
+print(subjects)
+subjects[3]='C++'
+print(subjects)
+a_=5
+print(a_)
