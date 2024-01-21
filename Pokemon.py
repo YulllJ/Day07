e23e8d144d66f019da60f1b399cff21031f0eba7
YulllJ <jyr8407@gmail.com>
2024-01-22 01:40:11 +0900

    v 0.4_2 Pokemon game -complete monster append

--- Pokemon.py
+++ Pokemon.py
@@ -1,16 +1,13 @@
 # 주말과제-포켓몬 게임 만들어오기
-# attack 진화 ...? text기반 게임 프로그램 시작하면 안내문구 야생의 누가 나타났다 이런거 player선택 뭐 이런거 ,,? 어뜨케 하라는겨
+# attack 진화 ...? text기반 게임 프로그램 시작하면 안내문구 야생의 누가 나타났다.
+# 이런거 player선택 뭐 이런거 ,,? 어뜨케 하라는겨
 # list, dictionary로 레벨별로 공격방법..? attack할지 모르는데
-# 포켓몬 도감..? 미완말고 최소한으로 만들어서 한다라던가
-# 도망도 하고 ..
+# 포켓몬 도감..? 미완말고 최소한으로 만들어서 한다라던가 도망도 하고 ..
 # 도망갔을때 랜덤하게 한다규..? 어케 한다는겨 도망가고 싶어도 못가는 경우도 있다..?
-# 기획해서 해보라는 거 인듯
-# 점수 메기기 보다는 수준 체크.. 예외처리도 확실히 해야한다.
+# 기획해서 해보라는 거 인듯   # 점수 메기기 보다는 수준 체크.. 예외처리도 확실히 해야한다.
 # 컴퓨터와 전투하는 포켓몬 게임 만들기
-
-
-
-
+# 물불풀 물이 불한테 강해 불은 풀한테 강해 풀은 물이 강해 상성에 맞게 hp감소는 1,2,3
+# 내가 0이면 졌습니다 끝나고 내가 이기면 상대방 캐릭터 나한테 저장
 
 class Player:
     def __init__(self,_id):
@@ -24,28 +21,31 @@ class Player:
         monster_name=input("Create your monster name: ")
         new_monster=Monster(monster_name,monster)
         self.my_monster.append(new_monster)
-        print(type(self.my_monster))
     def monster_dictionary(self):
         print(f"내가 가진 monster : ")
         try:
-            for i in self.my_monster :
-                print(i)
-                #i.information()
+            for i in self.my_monster:
+                print(f'name: [{i.name}] character: [{i.character}] level: [{i.level}]')
         except:
             print("monster도감 오류")
 
 
 class Monster:
     def __init__(self,name,character_pair):
+        #print(character_pair,type(character_pair))
         self.name = name
         self.character,self.type_skill=character_pair#dictionary로 받아야 character는 key, skill은 values
+        #print(type(self.character),type(self.type_skill)) string, tuple
         self.level = 0
         self.type_skill=list(self.type_skill)
         self.type, self.skill = self.type_skill[0], self.type_skill[1:]
+        self.hp = 10
     def attack(self,attack):
-        print(f"{self.name}이(가) {self.skill[attack]} 공격을 했다!")
+        print(f"{self.name}이(가) {attack} 공격을 했다!")
+    def attacked(self,monster_attack):
+        print(f"{self.name}이(가) {monster_attack} 공격을 받았다!")#여기 수정 요함
     def information(self):
-        print(f'name: [{self.name}] character: [{self.character}] level: [{self.level}]')
+        print(f'name: [{self.name}] character: [{self.character}] level: [{self.level}] hp: [{self.hp}]')
 def print_initial_state():
     print("Welcome to Pokemon World!")
     while True:
@@ -88,7 +88,7 @@ def set_player():
 def print_game():
 
     while True : #대문자 아님 주의...
-        menu=input("1.get monster 2.monster dictionary 3.quit game")
+        menu=input("1.싸운다 2.my pokemon 3.도망친다")
         if menu == '1':
             print("Start!")
             return 1
@@ -100,12 +100,90 @@ def print_game():
             pass
         else:
             print("Retry")
-def battle(player, monster):
-    print(f'야생의 {monster.name}이(가) 나타났다!!!')
-    print(monster.information())
-    for each_monster in player.my_monster:
-        print(each_monster.skill)
+def select(player):
+    print("Select a monster:")
+    for index, each_monster in enumerate(player.my_monster):
+        print(f"{index + 1}. {each_monster.name}")
 
+    while True:
+        try:
+            selected_index = int(input("Enter the number of the monster you want to select: ")) - 1
+            if 0 <= selected_index < len(player.my_monster):
+                selected_monster = player.my_monster[selected_index]
+                print(f"You selected: {selected_monster.name}")
+                return selected_monster
+            else:
+                print("Invalid index. Please enter a valid number.")
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
+
+def battle(player, monster,selected_monster):
+    print(monster.information())
+    while monster.hp > 0 or selected_monster.hp > 0:
+        print(f'name: {selected_monster.name} skill:{selected_monster.skill}')
+        choice = input("어떤 공격을 하시겠습니까? 1,2,3")
+        if choice == '1' :
+            selected_monster.attack(selected_monster.skill[0])
+            if (monster.type == '풀' and selected_monster.type == '물') or (
+                    monster.type == '불' and selected_monster.type == '풀') or (
+                    monster.type == '물' and selected_monster.type == '불'):
+                print("효과가 미미했다.")
+                monster.hp -= 1
+
+            elif (monster.type == '물' and selected_monster.type == '풀') or (
+                    monster.type == '풀' and selected_monster.type == '불') or (
+                    monster.type == '불' and selected_monster.type == '물'):
+                print("효과가 굉장했다.")
+                monster.hp -= 3
+
+            else:
+                monster.hp -= 2
+        elif choice == '2' :
+            selected_monster.attack(selected_monster.skill[1])
+            monster.hp -= 2
+        elif choice == '3' :
+            selected_monster.attack(selected_monster.skill[2])
+            monster.hp -= 2
+
+        else:
+            print("공격 if문에서 문제발생함")
+        print(f'{monster.name} HP :{monster.hp} {selected_monster.name} HP :{selected_monster.hp}')
+        if monster.hp <= 0 or selected_monster.hp <= 0: break
+
+        #AI공격 시작
+        random_number = random.randint(0, 2)
+        if random_number == 0 :
+            monster.attack(monster.skill[0])
+            if (monster.type == '풀' and selected_monster.type == '물') or (
+                    monster.type == '불' and selected_monster.type == '풀') or (
+                    monster.type == '물' and selected_monster.type == '불'):
+                print("효과가 굉장했다.")
+                selected_monster.hp -= 3
+            elif (monster.type == '물' and selected_monster.type == '풀') or (
+                    monster.type == '풀' and selected_monster.type == '불') or (
+                    monster.type == '불' and selected_monster.type == '물'):
+                print("효과가 미미했다.")
+                selected_monster.hp -= 1
+            else:
+                selected_monster.hp -= 2
+        elif random_number == 1 :
+            monster.attack(monster.skill[1])
+            selected_monster.hp -= 2
+        elif random_number == 2 :
+            monster.attack(monster.skill[2])
+            selected_monster.hp -= 2
+        else:
+            print("공격 if문에서 문제발생함")
+        print(f'{monster.name} HP :{monster.hp} {selected_monster.name} HP :{selected_monster.hp}')
+
+    if monster.hp <= 0 and selected_monster.hp > 0:
+        print(f'{player.name}이(가) 이겼습니다')
+        print(f'{player.name}이(가) {monster.character}를 획득합니다!')
+        new_monster=[monster.character,monster.type_skill]
+        player.get_monster(new_monster)
+    elif monster.hp <= 0 and selected_monster.hp > 0:
+        print(f'{player.name}이(가) 졌습니다')
 def main():
     state = "Start"
     global mode
@@ -122,22 +200,22 @@ def main():
                           '꼬부기': ("물", "물대포-물", "몸통박치기", "꼬리흔들기")}
         player1 = set_player()
         print("랜덤으로 몬스터를 한마리 갖습니다.")
-        print(player1.my_monster)
         initial_key = random.choice(list(character_dict.keys()))
         initial_pair = [initial_key, (character_dict[initial_key])]
-        player1.get_monster(initial_pair)
+        player1.get_monster(initial_pair)#여기서 프린트
         # player정보 setting끝
         print("Game Start!")
+        print("야생의 AI monster가 나타났다.")
+        selected_monster = select(player1)
         while True:
 
             game = print_game()
             if game ==1:
-                character_dict = {'이상해씨': ("풀", "덩굴채찍-풀", "몸통박치기", "울음소리")}
                 random_key = random.choice(list(character_dict.keys()))
                 character_pair = [random_key, (character_dict[random_key])]
                 AImonster = Monster("AImonster", character_pair)
                 # AI monster setting 끝
-                battle(player1,AImonster)
+                battle(player1,AImonster,selected_monster)
 
             elif game == 2:
                 player1.monster_dictionary()
